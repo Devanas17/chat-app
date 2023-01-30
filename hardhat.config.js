be1118e38a44f38368e4b0d18de6103c86994bcc
DevAnas17 <anasdeveloper17@gmail.com>
2023-01-30 22:58:00 +0530

    add apiFeature

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,7 @@ require("solidity-coverage");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
   networks: {
     goerli: {
       chainId: 5,
---
+++ vite-project/src/utils/apiFeature.jsx
@@ -0,0 +1,60 @@
+import { contractAddress, contractABI } from "./constant";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+
+export const checkIfWalletConnected = async () => {
+  try {
+    if (!window.ethereum) return alert("Please Install Metamask!");
+    const accounts = await window.ethereum.request({ method: "eth_accounts" });
+    const firstAccount = accounts[0];
+    return firstAccount;
+  } catch (error) {
+    console.log("Check If Wallet Connected:", error);
+  }
+};
+
+export const connectWallet = async () => {
+  try {
+    if (!window.ethereum) return alert("Please Install Metamask!");
+    const accounts = await window.ethereum.request({ method: "eth_accounts" });
+    const firstAccount = accounts[0];
+    return firstAccount;
+  } catch (error) {
+    console.log("Connect Wallet", error);
+  }
+};
+
+const fetchContract = async (provider) => {
+  new ethers.Contract(contractABI, contractAddress, provider);
+};
+
+export const connectWithContract = async () => {
+  try {
+    const web3modal = new Web3Modal();
+    const connection = await web3modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = fetchContract(signer);
+    return contract;
+  } catch (error) {
+    console.log("Connect With Contract", error);
+  }
+};
+
+export const convertTime = (time) => {
+  const newTime = new Date(time.toNumber());
+  const realTime =
+    newTime.getHours() +
+    "/" +
+    newTime.getMinutes() +
+    "/" +
+    newTime.getSeconds() +
+    "  Date:" +
+    newTime.getDate() +
+    "/" +
+    (newTime.getMonth() + 1) +
+    "/" +
+    newTime.getFullYear();
+
+    return realTime
+};
