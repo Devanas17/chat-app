013cace03a1aea7b97e6bef08000c0089e284235
DevAnas17 <anasdeveloper17@gmail.com>
2023-01-30 22:30:52 +0530

    ...

--- test/Chat.test.js
+++ test/Chat.test.js
@@ -11,10 +11,51 @@ describe("ChatApp", () => {
   });
 
   describe("Account Creation", async () => {
-    it("Should Create An Account", async () => {
-      let account = await chatApp.connect(user1).createAccount("Aman");
+    let account1, account2, account3;
+    beforeEach(async () => {
+      account1 = await chatApp.connect(user1).createAccount("Oggy");
+      account2 = await chatApp.connect(user2).createAccount("Jack");
+      account3 = await chatApp.connect(user3).createAccount("Nobita");
+    });
+    it("Should Check User Exists", async () => {
       const result = await chatApp.checkUserExists(await user1.getAddress());
       assert.equal(result, true, "User1 not created");
     });
+    it("Shoul get the Username", async () => {
+      const result = await chatApp.getUserName(await user1.getAddress());
+      expect(result).to.equal("Oggy");
+    });
+
+    it("Should return UserData", async () => {
+      const result = await chatApp.userLists(await user1.getAddress());
+      expect(result).to.equal("Oggy");
+    });
   });
+
+  describe("Add Friend", async () => {
+    let account1, account2, account3;
+    beforeEach(async () => {
+      account1 = await chatApp.connect(user1).createAccount("Oggy");
+      account2 = await chatApp.connect(user2).createAccount("Jack");
+      account3 = await chatApp.connect(user3).createAccount("Nobita");
+    });
+
+    it("Should Add Friend", async () => {
+      let addFriend = await chatApp
+        .connect(user1)
+        .addFriend(await user2.getAddress(), "Jack");
+    });
+
+    it("Should get all the users", async () => {
+      const result = await chatApp.getAllTheUser();
+      expect(result[0].name).to.equal("Oggy");
+      expect(result[1].name).to.equal("Jack");
+      expect(result[2].name).to.equal("Nobita");
+      expect(result[0].accountAddress).to.equal(await user1.getAddress());
+      expect(result[1].accountAddress).to.equal(await user2.getAddress());
+      expect(result[2].accountAddress).to.equal(await user3.getAddress());
+    });
+  });
+
+  describe("Send Message", async () => {});
 });
---
+++ vite-project/src/utils/constant.jsx
@@ -0,0 +1,229 @@
+export const contractAddress = ""
+export const contractABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_friend_key",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        }
+      ],
+      "name": "addFriend",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "bytes32",
+          "name": "",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "allMessages",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "sender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "timestamp",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "message",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_pubkey",
+          "type": "address"
+        }
+      ],
+      "name": "checkUserExists",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        }
+      ],
+      "name": "createAccount",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getAllTheUser",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "string",
+              "name": "name",
+              "type": "string"
+            },
+            {
+              "internalType": "address",
+              "name": "accountAddress",
+              "type": "address"
+            }
+          ],
+          "internalType": "struct ChatApp.AllUsers[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getMyFriendList",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "address",
+              "name": "pubkey",
+              "type": "address"
+            },
+            {
+              "internalType": "string",
+              "name": "name",
+              "type": "string"
+            }
+          ],
+          "internalType": "struct ChatApp.Friends[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_pubkey",
+          "type": "address"
+        }
+      ],
+      "name": "getUserName",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_friendKey",
+          "type": "address"
+        }
+      ],
+      "name": "readMessage",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "address",
+              "name": "sender",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "timestamp",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "message",
+              "type": "string"
+            }
+          ],
+          "internalType": "struct ChatApp.Message[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_friendKey",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "_message",
+          "type": "string"
+        }
+      ],
+      "name": "sendMessage",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "userLists",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
