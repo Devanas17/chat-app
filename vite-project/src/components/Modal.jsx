865ae0bfac09fd8db1565b33dbd292c76f35aaf5
DevAnas17 <anasdeveloper17@gmail.com>
2023-02-01 22:30:28 +0530

    add header & modal

--- vite-project/src/components/Modal.jsx
+++ vite-project/src/components/Modal.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from "react";
+import HH from "../assets/group-chat.svg";
+import { AppContext } from "../context/ChatAppContext";
+import { FiSend } from "react-icons/fi";
+import { BiUserCircle } from "react-icons/bi";
+import { FaRegAddressCard } from "react-icons/fa";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import Loader from "./Loader";
 
-const Modal = () => {
+const Modal = ({
+  openModal,
+  head,
+  heading,
+  subHeading,
+  title,
+  image,
+  address,
+  createAccount,
+}) => {
+  const [name, setName] = useState("");
+  const [accountAddress, setAccountAddress] = useState("");
+  const { loading } = useContext(AppContext);
+
   return (
-    <div>Modal</div>
-  )
-}
+    <div className="modal">
+      <div className="modal_box flex items-center justify-between px-8">
+        <div className="modal_box_left">
+          <img
+            src={image}
+            alt="Group Chat Banner"
+            className="h-[400px] w-[400px]"
+          />
+        </div>
+        <div className="modal_box_RIGHT">
+          <h1 className="">
+            {title} <span>{head}</span>{" "}
+          </h1>
+          <p className="">{heading}</p>
+          <small>{subHeading}</small>
 
-export default Modal
+          {loading == true ? (
+            <Loader />
+          ) : (
+            <div className="modal_right_name">
+              <div className=" border flex items-center p-2 space-x-3">
+                <BiUserCircle className="h-6 w-6" />
+                <input
+                  type="text"
+                  className="border-none outline-none w-full"
+                  placeholder="Your name "
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className=" flex items-center space-x-3 border p-2">
+                <FaRegAddressCard className="w-6 h-6" />
+                <input
+                  type="text"
+                  className="border-none outline-none w-full"
+                  placeholder={address || "Enter Address.."}
+                  onChange={(e) => setAccountAddress(e.target.value)}
+                />
+              </div>
+
+              <div className="flex items-center space-x-8 mt-5">
+                <button
+                  className="border flex items-center px-6 py-2"
+                  onClick={() => createAccount({ name, accountAddress })}
+                >
+                  <FiSend className="h-4 w-4 " />
+                  <span className="ml-2">Send</span>
+                </button>
+                <button
+                  className="border flex items-center px-6 py-2"
+                  onClick={() => openModal(false)}
+                >
+                  <AiOutlineCloseCircle className="h-4 w-4 " />
+                  <span className="ml-2">Cancel</span>
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -3,26 +3,130 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 import { AppContext } from "../context/ChatAppContext";
 import Modal from "./Modal";
 import Error from "./Error";
-import { AiFillWechat } from "react-icons/ai";
+import {
+  AiFillWechat,
+  AiOutlineCloseCircle,
+  AiOutlineMenu,
+} from "react-icons/ai";
+import Logo from "../assets/react.svg";
+import GroupChat from "../assets/group-chat.svg"
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const { connectWallet, userName, currentAccount } = useContext(AppContext);
+  const { connectWallet, userName, currentAccount,  createAccount, error } = useContext(AppContext);
   const [isActive, setIsActive] = useState(2);
   const [toggle, setToggle] = useState(false);
-  const [openModal, setOpenModal] = useState(false)
+  const [openModal, setOpenModal] = useState(false);
 
+  const menuItems = [
+    { name: "All Users", link: "/alluser" },
+    { name: "Chat", link: "/" },
+    { name: "Contact", link: "/" },
+    { name: "Settings", link: "/" },
+    { name: "FAQs", link: "/" },
+    // { name: "Terms of Use", link: "/" },
+  ];
 
-    const menuItems = [
-      { menu: "All Users", link: "/alluser" },
-      { menu: "Chat", link: "/" },
-      { menu: "Contact", link: "/" },
-      { menu: "Settings", link: "/" },
-      { menu: "FAQs", link: "/" },
-      { menu: "Terms of Use", link: "/" },
-    ];
+  return (
+    <div className="relative">
+      <div className="nav_box flex items-center justify-between px-6 py-3">
+        <div className="nav_box_left">
+          <AiFillWechat className="text-blue-700 h-12 w-12" />
+        </div>
 
-  return <div className=""></div>;
+        <div className="nav_box_right flex items-center ">
+          <div className="nav_box_right_menu hidden sm:flex space-x-7">
+            {menuItems.map((menu, i) => (
+              <div
+                onClick={() => setIsActive(i + 1)}
+                key={i + 1}
+                className={`nav_box_right_menu_item  hover:text-orange-600 flex ${
+                  isActive == i + 1 ? "active_btn text-orange-700 " : " "
+                }`}
+              >
+                <Link
+                  className="nav_box_right_menu_item_link uppercase text-sm font-normal"
+                  to={menu.link}
+                >
+                  {menu.name}
+                </Link>
+              </div>
+            ))}
+          </div>
+
+          {/* Mobile */}
+          {toggle ? (
+            <div className="nav_box_mobile_menu absolute top-0 right-0 flex flex-col bg-white border-2 border-gray-300 px-10 py-4 space-y-5">
+              {menuItems.map((menu, i) => (
+                <div
+                  onClick={() => setIsActive(i + 1)}
+                  key={i + 1}
+                  className={`nav_box_mobile_menu_item font-medium hover:text-orange-600 flex ${
+                    isActive == i + 1 ? "active_btn text-orange-700 " : " "
+                  }`}
+                >
+                  <Link
+                    className="nav_box_mobile_menu_item_link uppercase "
+                    to={menu.link}
+                  >
+                    {menu.name}
+                  </Link>
+                </div>
+              ))}
+
+              <div className="mobile_mne_btn">
+                <AiOutlineCloseCircle
+                  className="h-14 w-14 cursor-pointer "
+                  onClick={() => setToggle(false)}
+                />
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+
+          {/* Connect Wallet */}
+          <div className="conect-btn">
+            {currentAccount ? (
+              <button onClick={() => connectWallet()}>Connect Wallet</button>
+            ) : (
+              <button
+                onClick={() => setOpenModal(true)}
+                className="ml-5 border-2 px-5 py-2"
+              >
+                <img src={userName ? Logo : "H"} alt="" className="" />
+                <small>{userName || "Create Account"}</small>
+              </button>
+            )}
+          </div>
+
+          <div
+            className="nav_box_right_open sm:hidden ml-3"
+            onClick={() => setToggle(true)}
+          >
+            <AiOutlineMenu className="w-10 h-10 cursor-pointer" />
+          </div>
+        </div>
+      </div>
+
+      {/* Modal Component */}
+      {openModal && (
+        <div className="">
+          <Modal
+            openModal={setOpenModal}
+            title="Welcome"
+            head="Chat Buddy"
+            heading="You can get everything in life you want if you will just help enough other people get what they want."
+            subHeading="Kindly select your name"
+            image={GroupChat}
+            createAccount={ createAccount}
+            address={currentAccount}
+          />
+        </div>
+      )}
+      {error ? <Error error={error}/> : ""}
+    </div>
+  );
 };
 
 export default Navbar;
